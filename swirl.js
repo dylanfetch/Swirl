fd20a3b645a0edd553139b72b987c016e23ac1ec
dylanfetch <dylanfetch@gmail.com>
2019-03-10 14:47:52 -0400

    Replaced HTML function
    Replaced Circle function HTML by adding an element property and creating
    the circle element in the constructor

--- swirl.js
+++ swirl.js
@@ -2,27 +2,23 @@ window.onload = function(){
   var add_circle_button = document.getElementById("add_circle");
   var swirl = new Swirl()
   var circle = new Circle(1,300,"#000000")
-  //add_circle_button.addEventListener("click", function(){add_circle()});
   add_circle_button.addEventListener("click", function(){swirl.addCircle(circle)});
 };
-
-function add_circle(){
-  var container = document.getElementById("swirl_container");
-  container.innerHTML = "<div class='circle' style='height: 300px; width: 300px; background-color: #000000;'></div>";
-  var dashboard = document.getElementById("dashboard");
-  var circle_info = document.createElement("div");
-  circle_info.setAttribute("class", "circle_info");
-  dashboard.appendChild(circle_info);
-}
 
 class Circle {
   id;
   diameter;
   color;
+  element;
   constructor(id, diameter, color){
     this.id = id;
     this.diameter = diameter;
     this.color = color;
+    this.element = document.createElement("div");
+    this.element.setAttribute("class", "circle");
+    this.element.setAttribute("style", "height:"+diameter+
+                                       "px; width:"+diameter+
+                                       "px; background-color:"+color+";");
   }
   get id() {
     return this.id;
@@ -32,6 +28,9 @@ class Circle {
   }
   get color() {
     return this.color;
+  }
+  get element() {
+    return this.element;
   }
   get HTML() {
     return "<div class='circle" +
@@ -51,7 +50,8 @@ class Swirl {
     this.dashboard = document.getElementById("dashboard");
   }
   addCircle(circle){
-    this.container.innerHTML = circle.HTML;
+    //this.container.innerHTML = circle.HTML;
+    this.container.appendChild(circle.element);
     var circle_info = document.createElement("div");
     circle_info.setAttribute("class", "circle_info");
     this.dashboard.appendChild(circle_info);
