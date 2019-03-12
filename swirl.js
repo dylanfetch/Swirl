4e253e6df7e80eb3caf0f248e5841011d486441d
dylanfetch <dylanfetch@gmail.com>
2019-03-12 16:59:57 -0400

    Diameters and colors are changeable now
    Circle_info cards now have inputs with event listeners that set the
    value of the diameter and color to the value of the input on keyup

--- swirl.js
+++ swirl.js
@@ -31,8 +31,40 @@ class Circle {
   createInfoElement(id, diameter, color){
     let $self = this;
     var info_element = document.createElement("div");
-    info_element.setAttribute("class", "circle_info")
-    info_element.innerHTML = "Id: "+id+"</br>Diameter: "+diameter+"px</br>Color: "+color;
+    info_element.setAttribute("class", "circle_info");
+    //info_element.innerHTML = "Id: "+id+"</br>Diameter: "+diameter+"px</br>Color: "+color;
+
+    var diameter_container = document.createElement("div");
+    var diameter_label = document.createElement("label");
+    diameter_label.setAttribute("for","Diameter");
+    diameter_label.innerHTML = "Diameter:&nbsp;&nbsp;";
+    var diameter_input = document.createElement("input");
+    diameter_input.setAttribute("name", "Diameter");
+    diameter_input.setAttribute("value", $self.diameter);
+    diameter_input.setAttribute("size", 16);
+    diameter_input.addEventListener("keyup",function(){$self.setDiameter()});
+    diameter_container.appendChild(diameter_label);
+    diameter_container.appendChild(diameter_input);
+
+    var color_container = document.createElement("div");
+    var color_label = document.createElement("label");
+    color_label.setAttribute("for","Color");
+    color_label.innerHTML = "Color:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+    var color_input = document.createElement("input");
+    color_input.setAttribute("name", "Color");
+    color_input.setAttribute("value", $self.color);
+    color_input.setAttribute("size", 16);
+    color_input.addEventListener("keyup",function(){$self.setColor()});
+    color_container.appendChild(color_label);
+    color_container.appendChild(color_input);
+
+    //info_element.appendChild(diameter_label);
+    //info_element.appendChild(diameter_input);
+    info_element.appendChild(diameter_container);
+    info_element.appendChild(color_container);
+    //info_element.appendChild(color_label);
+    //info_element.appendChild(color_input);
+
     var x_button = document.createElement("div");
     x_button.setAttribute("class", "x_button");
     x_button.innerHTML = "X";
@@ -43,6 +75,17 @@ class Circle {
 
   deleteSelf(){
     this.parent.deleteCircle(this.id);
+  }
+
+  setDiameter(){
+    this.diameter = this.info_element.children[0].children[1].value;
+    this.element.style["height"] = this.diameter + "px";
+    this.element.style["width"] = this.diameter + "px";
+  }
+
+  setColor(){
+    this.color = this.info_element.children[1].children[1].value;
+    this.element.style["background-color"] = this.color;
   }
 }
 
@@ -82,9 +125,10 @@ class Swirl {
   }
 
   createCircle(id, diameter, color, parent){
-    var circle = new Circle(id, diameter, color, parent);
     let $self = this;
+    var circle = new Circle(id, diameter, color, parent);
     var key_array = Object.keys(this.circle_array);
+
     if(key_array.length){
       var max_key = key_array[key_array.length-1]
       var last_circle = this.circle_array[max_key];
@@ -102,6 +146,7 @@ class Swirl {
     var element = this.circle_array[string_id].element;
     var info_element = this.circle_array[string_id].info_element;
     var children = element.firstChild;
+
     if(children){
       element.parentNode.appendChild(children);
     }
