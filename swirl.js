219d70fdcfc7c56abfea93150e456156c31a622e
dylanfetch <dylanfetch@gmail.com>
2019-03-13 20:29:27 -0400

    Made code more readable
    Broke up larger methods into smaller methods, added whitespace

--- swirl.js
+++ swirl.js
@@ -2,93 +2,6 @@ window.onload = function(){
   var swirl = new Swirl()
 };
 
-class Circle {
-  id;
-  diameter;
-  color;
-  element;
-  info_element;
-  parent;
-
-  constructor(id, diameter, color, parent){
-    this.id = id;
-    this.diameter = diameter;
-    this.color = color;
-    this.parent = parent;
-    this.element = this.createCircleElement(diameter, color);
-    this.info_element = this.createInfoElement(id, diameter, color);
-  }
-
-  createCircleElement(diameter, color){
-    var element = document.createElement("div");
-    element.setAttribute("class", "circle");
-    element.setAttribute("style", "height:"+diameter+
-                                  "px; width:"+diameter+
-                                  "px; background-color:"+color+";");
-    return element;
-  }
-
-  createInfoElement(id, diameter, color){
-    let $self = this;
-    var info_element = document.createElement("div");
-    info_element.setAttribute("class", "circle_info");
-    //info_element.innerHTML = "Id: "+id+"</br>Diameter: "+diameter+"px</br>Color: "+color;
-
-    var diameter_container = document.createElement("div");
-    var diameter_label = document.createElement("label");
-    diameter_label.setAttribute("for","Diameter");
-    diameter_label.innerHTML = "Diameter:&nbsp;&nbsp;";
-    var diameter_input = document.createElement("input");
-    diameter_input.setAttribute("name", "Diameter");
-    diameter_input.setAttribute("value", $self.diameter);
-    diameter_input.setAttribute("size", 16);
-    diameter_input.addEventListener("keyup",function(){$self.setDiameter()});
-    diameter_container.appendChild(diameter_label);
-    diameter_container.appendChild(diameter_input);
-
-    var color_container = document.createElement("div");
-    var color_label = document.createElement("label");
-    color_label.setAttribute("for","Color");
-    color_label.innerHTML = "Color:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-    var color_input = document.createElement("input");
-    color_input.setAttribute("name", "Color");
-    color_input.setAttribute("value", $self.color);
-    color_input.setAttribute("size", 16);
-    color_input.addEventListener("keyup",function(){$self.setColor()});
-    color_container.appendChild(color_label);
-    color_container.appendChild(color_input);
-
-    //info_element.appendChild(diameter_label);
-    //info_element.appendChild(diameter_input);
-    info_element.appendChild(diameter_container);
-    info_element.appendChild(color_container);
-    //info_element.appendChild(color_label);
-    //info_element.appendChild(color_input);
-
-    var x_button = document.createElement("div");
-    x_button.setAttribute("class", "x_button");
-    x_button.innerHTML = "X";
-    x_button.addEventListener("click",function(){$self.deleteSelf()});
-    info_element.appendChild(x_button);
-    return info_element;
-  }
-
-  deleteSelf(){
-    this.parent.deleteCircle(this.id);
-  }
-
-  setDiameter(){
-    this.diameter = this.info_element.children[0].children[1].value;
-    this.element.style["height"] = this.diameter + "px";
-    this.element.style["width"] = this.diameter + "px";
-  }
-
-  setColor(){
-    this.color = this.info_element.children[1].children[1].value;
-    this.element.style["background-color"] = this.color;
-  }
-}
-
 
 class Swirl {
   add_circle_button;
@@ -153,5 +66,141 @@ class Swirl {
     element.parentNode.removeChild(element);
     info_element.parentNode.removeChild(info_element);
     delete this.circle_array[string_id];
+  }
+}
+
+
+class Circle {
+  id;
+  diameter;
+  color;
+  element;
+  info_element;
+  parent;
+
+  constructor(id, diameter, color, parent){
+    this.id = id;
+    this.diameter = diameter;
+    this.color = color;
+    this.parent = parent;
+    this.element = this.createCircleElement(diameter, color);
+    this.info_element = this.createInfoElement(id, diameter, color);
+  }
+
+  createCircleElement(diameter, color){
+    var element = document.createElement("div");
+    element.setAttribute("class", "circle");
+    element.setAttribute("style", "height:"+diameter+
+                                  "px; width:"+diameter+
+                                  "px; background-color:"+color+";");
+    return element;
+  }
+
+  createInfoElement(id, diameter, color){
+    var info_element = document.createElement("div");
+    info_element.setAttribute("class", "circle_info");
+
+    var diameter_container = this.createDiameterContainer();
+    var color_container = this.createColorContainer();
+    var x_button = this.createXButton();
+
+    info_element.appendChild(diameter_container);
+    info_element.appendChild(color_container);
+    info_element.appendChild(x_button);
+
+    return info_element;
+  }
+
+  createDiameterContainer(){
+    var diameter_container = document.createElement("div");
+
+    var diameter_label = this.createDiameterLabel();
+    var diameter_input = this.createDiameterInput();
+
+    diameter_container.appendChild(diameter_label);
+    diameter_container.appendChild(diameter_input);
+
+    return diameter_container;
+  }
+
+  createColorContainer(){
+    var color_container = document.createElement("div");
+
+    var color_label = this.createColorLabel();
+    var color_input = this.createColorInput();
+
+    color_container.appendChild(color_label);
+    color_container.appendChild(color_input);
+
+    return color_container;
+  }
+
+  createDiameterLabel(){
+    var diameter_label = document.createElement("label");
+
+    diameter_label.setAttribute("for","Diameter");
+    diameter_label.innerHTML = "Diameter:&nbsp;&nbsp;";
+
+    return diameter_label;
+  }
+
+  createDiameterInput(){
+    let $self = this;
+    var diameter_input = document.createElement("input");
+
+    diameter_input.setAttribute("name", "Diameter");
+    diameter_input.setAttribute("value", $self.diameter);
+    diameter_input.setAttribute("size", 16);
+    diameter_input.addEventListener("keyup",function(){$self.setDiameter()});
+
+    return diameter_input;
+  }
+
+  createColorLabel(){
+    var color_label = document.createElement("label");
+
+    color_label.setAttribute("for","Color");
+    color_label.innerHTML = "Color:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+
+    return color_label;
+  }
+
+  createColorInput(){
+    let $self = this;
+    var color_input = document.createElement("input");
+
+    color_input.setAttribute("name", "Color");
+    color_input.setAttribute("value", $self.color);
+    color_input.setAttribute("type", "color");
+    color_input.setAttribute("size", 16);
+    color_input.addEventListener("keyup",function(){$self.setColor()});
+
+    return color_input;
+  }
+
+  createXButton(){
+    let $self = this;
+    var x_button = document.createElement("div");
+
+    x_button.setAttribute("class", "x_button");
+    x_button.innerHTML = "X";
+    x_button.addEventListener("click",function(){$self.deleteSelf()});
+
+    return x_button;
+  }
+
+  deleteSelf(){
+    this.parent.deleteCircle(this.id);
+  }
+
+  setDiameter(){
+    this.diameter = this.info_element.children[0].children[1].value;
+    this.element.style["height"] = this.diameter + "px";
+    this.element.style["width"] = this.diameter + "px";
+  }
+
+  setColor(){
+    this.color = this.info_element.children[1].children[1].value;
+    this.element.style["background-color"] = this.color;
   }
 }
